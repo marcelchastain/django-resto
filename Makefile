0c746f970a7d420ecf915a063a5bd426a27406a3
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-08-25 22:51:32 +0200

    Tweak Makefile and setup.py for Python 3.

--- Makefile
+++ Makefile
@@ -10,4 +10,5 @@ coverage:
 
 clean:
 	find . -name '*.pyc' -delete
+	find . -name __pycache__ -delete
 	rm -rf django_resto/tests/media .coverage dist htmlcov MANIFEST
--- setup.py
+++ setup.py
@@ -32,10 +32,11 @@ distutils.core.setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        "Topic :: Internet :: WWW/HTTP :: Site Management",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
     ],
     platforms='all',
     license='BSD'
